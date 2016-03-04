2cb3fca64fa4aee0daf20d0b0e25ca506f7387e3
Cameron Pierson <cjaypierson@gmail.com>
2016-03-03 21:25:18 -0500

    Setup Player 1 & 2 and route for battle

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,25 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+var ConfirmBattleContainer = React.createClass({
+	contextTypes: {
+		router: React.PropTypes.object.isRequired
+	},
+	getInitialState: function () {
+		return {
+			isLoading: true,
+			playerInfo: []
+		}
+	},
+	componentDidMount: function () {
+		var query = this.props.location.query;
+		// Fetch info from github then update the state
+	},
+	render: function() {
+		return (
+			<ConfirmBattle />
+		);
+	}
+});
+
+module.exports = ConfirmBattleContainer;
